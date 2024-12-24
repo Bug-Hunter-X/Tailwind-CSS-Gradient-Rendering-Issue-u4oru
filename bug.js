cdbd83cb4c41cd9fbec1343eb1424db6e3beb3fc
BugHunter-X <askzenai@gmail.com>
2024-12-24 15:44:40 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,8 @@
+```javascript
+// ... your Tailwind CSS classes ...
+<div class="bg-gradient-to-r from-blue-500 to-purple-500">
+  <!-- Content here -->
+</div>
+```
+
+This code might produce unexpected results if the `to` color in the gradient is not specified correctly or if the browser doesn't support the gradient property well.
